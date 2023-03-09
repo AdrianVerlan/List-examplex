bfe66028fa73ade5de0843ef41e2cee8bb9a0958
AdrianVerlan <123242162+AdrianVerlan@users.noreply.github.com>
2023-03-09 21:23:31 +0200

    Updated using while loop

--- weather_HW.py
+++ weather_HW.py
@@ -1,10 +1,11 @@
 from os import system
-
+system("cls")
 days = ["MO","TU","Wd","TH","FR","SA","SU"]
 temps =[10.9, 11.7, 17.9, 8, -18, 15.0, -6]
-
-for i in range(7):
+i =0
+while i<7:
     if temps[i]<=0:
         sign="*"
     else: sign =" "
     print(f"|",sign, days[i],"|",temps[i])
+    i+=1
