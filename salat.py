726310481ee50e853287e901e17dbf7c693f55fd
AdrianVerlan <123242162+AdrianVerlan@users.noreply.github.com>
2023-03-11 12:26:01 +0200

    Salad HW

---
+++ salat.py
@@ -0,0 +1,8 @@
+from os import system
+
+food_name= ["Salad", "Soup", "Bread"]
+food_price= [15.00, 12.00, 2.50, 30.00 ]
+
+food_name.append("Burgher")
+for i in range (4):
+    print(food_name[i],food_price[i])
