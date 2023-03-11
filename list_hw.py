3d02d137568e9d156f05a2308162254148c9e041
AdrianVerlan <123242162+AdrianVerlan@users.noreply.github.com>
2023-03-11 16:38:54 +0200

    List HW using for loop

--- list_hw.py
+++ list_hw.py
@@ -1,5 +1,11 @@
 from os import system
 system("cls")
-L= [1,2,3,4,5]
-L.reverse()
+L = [1, 2, 3, 4, 5]
+
+for i in range(len(L)//2):
+    temp = L[i]
+    L[i] = L[-i-1]
+    L[-i-1] = temp
+
+
 print(L)
